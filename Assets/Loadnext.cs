07feaafe9423799a10892c5fc1793b2813d0d33f
RaptorDOS <43664460+RaptorDOS@users.noreply.github.com>
2019-09-26 11:16:43 +0530

    Add files via upload

---
+++ Assets/Loadnext.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Loadnext : MonoBehaviour
+{
+    public void LoadNextLevel()
+	{ SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); }//Will load the next scene in the Index queue
+}
